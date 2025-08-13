2301f529ccd9c5a76bb68890c0c9c6609631a71a
PataBasket <basket2323yuta@gmail.com>
2025-08-14 02:57:41 +0900

    Add: :lipstick: image field prepared

--- Assets/Scripts/MovementManager.cs
+++ Assets/Scripts/MovementManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class MovementManager : MonoBehaviour
 {
@@ -7,13 +8,22 @@ public class MovementManager : MonoBehaviour
 
     [SerializeField] 
     private float rotationSpeed;
+    [SerializeField] float skyboxDuration;
 
     private bool _isRotating = true;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        var mat = new Material(RenderSettings.skybox);
+        RenderSettings.skybox = mat;
+        DOTween.To(
+                () => mat.GetFloat("_Rotation"),
+                v  => mat.SetFloat("_Rotation", v),
+                360f, skyboxDuration
+            )
+            .SetEase(Ease.Linear)
+            .SetLoops(-1, LoopType.Incremental); // ずっと回す
     }
 
     // Update is called once per frame
